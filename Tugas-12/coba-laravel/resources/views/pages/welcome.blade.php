138418eddb06544ff39f1fa394ab0656fd23f025
Rezy <ryzerix@gmail.com>
2024-02-02 22:21:55 +0700

    restructuring folders 2: Electric Boogaloo

--- Tugas-12/coba-laravel/resources/views/pages/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.master')
-@section('judul', 'Home')
-@section('content')
-    <h1>Selamat Datang {{$Fname}} {{$Lname}}!</h1>
-    <h2>Terima kasih telah bergabung di SanberBook. Social Media kita bersama!</h2>
-@endsection
